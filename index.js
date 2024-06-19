d2d61f7f9f5802f7fdf83b3605b071da1859fb74
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-06-20 01:27:14 +0500

    home route with a simple message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const formController = require('./controllers/formController');
+const homeController = require('./controllers/homeController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
+
+app.get('/', homeController.helloWorld);
 
 app.post('/submit', formController.submitForm);
 
