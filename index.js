4d61a4c4537cc02a3068906fd172632dba15d700
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-06-21 03:07:52 +0500

    allowed cors in heroku-single-service

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const formController = require('./controllers/formController');
 const homeController = require('./controllers/homeController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
